1bd7468b34299e5c686a021ba9f4fa4b1d85e313
Durjoy96 <mddurjoy853@gmail.com>
2024-11-04 23:30:17 +0600

    footer complete

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/header/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import Footer from "../components/footer/Footer";
 
 const Root = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
