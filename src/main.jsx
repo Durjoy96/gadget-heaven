1b7bcc19aeb08808fceeab0bfe5c8d00ee6345dd
Durjoy96 <mddurjoy853@gmail.com>
2024-11-06 14:04:17 +0600

    statistics page complete

--- src/main.jsx
+++ src/main.jsx
@@ -58,6 +58,7 @@ const router = createBrowserRouter([
       {
         path: "/statistics",
         element: <Statistics></Statistics>,
+        loader: () => fetch("/gadgetsData.json"),
       },
       {
         path: "/dashboard",
